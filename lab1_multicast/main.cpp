b9bbdd2604cbe0d53267d512315d6b1dc10aa67c
mentalMint <walkershush@gmail.com>
2022-09-24 13:01:44 +0700

    Change timing

--- lab1_multicast/main.cpp
+++ lab1_multicast/main.cpp
@@ -15,10 +15,11 @@
 #define PORT 12000
 #define BUF_SIZE 1
 #define PERIOD_SEC 3
+#define CONNECTION_TIME_MICROSEC 100000
 #define INTERFACE "enp0s3"
-#define DEFAULT_STATUS 0
-#define CONNECT_STATUS 1
-#define EXIT_STATUS 2
+#define STATUS_DEFAULT 0
+#define STATUS_CONNECT 1
+#define STATUS_EXIT 2
 
 int socket_fd;
 struct sockaddr_in addr{};
@@ -34,17 +35,22 @@ struct StringsComparator {
 
 void print_map(const std::map<char*, bool, StringsComparator> &map) {
     for (auto n: map) {
-            printf("%s\n", n.first);
+        printf("%s\n", n.first);
     }
 }
 
-void set_select(fd_set* readfds, struct timeval* tv) {
+void set_select(fd_set* readfds, struct timeval* tv, bool connection) {
     if (FD_ISSET(socket_fd, readfds) == 0) {
         FD_ZERO(readfds);
         FD_SET(socket_fd, readfds);
     }
-    tv->tv_sec = PERIOD_SEC;
-    tv->tv_usec = 0;
+    if (connection) {
+        tv->tv_sec = 0;
+        tv->tv_usec = CONNECTION_TIME_MICROSEC;
+    } else {
+        tv->tv_sec = PERIOD_SEC;
+        tv->tv_usec = 0;
+    }
 }
 
 void close_all() {
@@ -52,7 +58,7 @@ void close_all() {
 }
 
 void leave() {
-    send_buf[0] = EXIT_STATUS;
+    send_buf[0] = STATUS_EXIT;
     if (-1 == sendto(socket_fd, send_buf, BUF_SIZE, 0,
                      (struct sockaddr*) &(addr), (socklen_t) addrlen)) {
         perror("sendto");
@@ -102,9 +108,9 @@ void remove_offline_users(std::map<char*, bool, StringsComparator> &users_online
     }
 }
 
-void check_need_for_send(struct timespec* timer1_start, struct timespec* timer1_end, int* recv_buf) {
+void check_need_for_send(struct timespec* timer1_start, struct timespec* timer1_end, const int* recv_buf) {
     clock_gettime(CLOCK_MONOTONIC_RAW, timer1_end);
-    if (timer1_end->tv_sec - timer1_start->tv_sec >= PERIOD_SEC || recv_buf[0] == CONNECT_STATUS) {
+    if (timer1_end->tv_sec - timer1_start->tv_sec >= PERIOD_SEC || recv_buf[0] == STATUS_CONNECT) {
         send_datagram();
         set_addr();
         clock_gettime(CLOCK_MONOTONIC_RAW, timer1_start);
@@ -114,9 +120,27 @@ void check_need_for_send(struct timespec* timer1_start, struct timespec* timer1_
 void check_remove_timer(struct timespec* timer2_start, struct timespec* timer2_end,
                         std::map<char*, bool, StringsComparator> &users_online) {
     clock_gettime(CLOCK_MONOTONIC_RAW, timer2_end);
-    if (timer2_end->tv_sec - timer2_start->tv_sec >= PERIOD_SEC * 4) {
+    if (timer2_end->tv_sec - timer2_start->tv_sec >= PERIOD_SEC * 3) {
         remove_offline_users(users_online);
         clock_gettime(CLOCK_MONOTONIC_RAW, timer2_start);
+    }
+}
+
+void print_users_online(const std::map<char*, bool, StringsComparator> &users_online) {
+    printf("===Users online===\n");
+    print_map(users_online);
+    printf("==================\n\n");
+    fflush(stdout);
+}
+
+void check_connection_timer(bool* connection, struct timespec* timer3_start, struct timespec* timer3_end,
+                            const std::map<char*, bool, StringsComparator> &users_online) {
+    if (*connection) {
+        clock_gettime(CLOCK_MONOTONIC_RAW, timer3_end);
+        if (timer3_end->tv_nsec - timer3_start->tv_nsec > CONNECTION_TIME_MICROSEC * 1000) {
+            print_users_online(users_online);
+            *connection = false;
+        }
     }
 }
 
@@ -174,28 +198,31 @@ int main() {
 
     //SELECT
     fd_set readfds;
-    struct timeval tv{};
-    int retval;
-
     int recv_buf[BUF_SIZE];
     memset(recv_buf, 0, BUF_SIZE);
 
     set_addr();
 
     std::map<char*, bool, StringsComparator> users_online;
-    send_buf[0] = CONNECT_STATUS;
+
+    send_buf[0] = STATUS_CONNECT;
     send_datagram();
-    send_buf[0] = DEFAULT_STATUS;
+    send_buf[0] = STATUS_DEFAULT;
+
+    bool connection = true;
+    struct timeval tv{};
+
     struct timespec timer1_start{}, timer1_end{};
     struct timespec timer2_start{}, timer2_end{};
+    struct timespec timer3_start{}, timer3_end{};
     clock_gettime(CLOCK_MONOTONIC_RAW, &timer1_start);
     clock_gettime(CLOCK_MONOTONIC_RAW, &timer2_start);
+    clock_gettime(CLOCK_MONOTONIC_RAW, &timer3_start);
 
     while (true) {
         set_addr();
-        set_select(&readfds, &tv);
-        retval = select(socket_fd + 1, &readfds, NULL, NULL, &tv);
-
+        set_select(&readfds, &tv, connection);
+        int retval = select(socket_fd + 1, &readfds, nullptr, nullptr, &tv);
         if (retval == -1) {
             perror("select");
             break;
@@ -206,7 +233,6 @@ int main() {
                 perror("recvfrom");
                 break;
             }
-
             user_address_tmp = inet_ntoa(addr.sin_addr);
             if (strcmp(user_address_tmp, host_address) != 0) {
                 bool contains = false;
@@ -216,31 +242,32 @@ int main() {
                         break;
                     }
                 }
-                if (recv_buf[0] != EXIT_STATUS) {
+                if (recv_buf[0] != STATUS_EXIT) {
                     if (!contains) {
                         char* user_address = (char*) malloc(20);
                         strcpy(user_address, user_address_tmp);
                         users_online.insert({user_address, true});
-                        printf("==New user online==\n");
-                        printf("===Users online===\n");
-                        print_map(users_online);
-                        printf("==================\n\n");
-                        fflush(stdout);
+                        if (!connection) {
+                            printf("==New user online==\n");
+                            print_users_online(users_online);
+                        }
                     } else {
                         users_online[user_address_tmp] = true;
                     }
+                    check_connection_timer(&connection, &timer3_start, &timer3_end, users_online);
                 } else {
                     users_online.erase(user_address_tmp);
                     printf("==Some users left==\n");
-                    printf("===Users online===\n");
-                    print_map(users_online);
-                    printf("==================\n\n");
-                    fflush(stdout);
+                    print_users_online(users_online);
                 }
                 check_need_for_send(&timer1_start, &timer1_end, recv_buf);
                 check_remove_timer(&timer2_start, &timer2_end, users_online);
             }
         } else {
+            if (connection) {
+                print_users_online(users_online);
+                connection = false;
+            }
             check_remove_timer(&timer2_start, &timer2_end, users_online);
             send_datagram();
         }
